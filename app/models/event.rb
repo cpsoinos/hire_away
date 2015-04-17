fdd05e2797160f852762bdc5e16fc995f70988f9
Corey Psoinos <coreypsoinos@gmail.com>
2015-04-17 14:16:04 -0400

    Google OAuth

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,19 @@ class Event < ActiveRecord::Base
 
   validates :name, presence: true
   validates :venue, presence: true
+
+  def self.add_to_calendar(new_event)
+    RiCal.Calendar do |cal|
+      cal.event do |event|
+        event.description = name
+        event.dtstart =  start_time
+        event.dtend = end_time
+        event.location = venue.name
+        # event.add_attendee "john.glenn@nasa.gov"
+        # event.alarm do
+        #   description "Segment 51"
+        # end
+      end
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
+
   has_many :offers
   has_many :calls
   has_many :positions, through: :calls
@@ -14,5 +16,20 @@ class User < ActiveRecord::Base
 
   def admin?
     role == "admin"
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.full_name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+    end
+  end
+
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(email: data["email"]).first
+    user
   end
 end
